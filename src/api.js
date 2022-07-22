02598b972e85737e0e5a4f5a4c0ccb5fb08fec9e
DESKTOP-Q9QG468\HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2022-07-23 01:53:13 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6496975b62msh62b90910d690ce0p1122a1jsncd4223b8d08e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
